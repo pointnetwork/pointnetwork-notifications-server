4c4b1ee0e955a0080ed987b41e673a1c2261438d
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-05-31 15:27:22 +0530

    Set proxy and test connecting to / route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 function App() {
+  fetch('/').then(console.log).catch(console.log);
+
   return (
     <Box>
       <Typography>Hello World</Typography>
