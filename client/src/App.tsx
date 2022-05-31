c018cd8ce73ed33f2e6dd8acc0c67dc99d51b9c0
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-05-31 15:20:37 +0530

    Scaffold the client

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+function App() {
+  return (
+    <Box>
+      <Typography>Hello World</Typography>
+    </Box>
+  );
+}
+
+export default App;
