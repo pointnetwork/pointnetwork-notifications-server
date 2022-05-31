e6be6a6737a455de3253d079debfa48c564adb08
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-05-31 16:46:05 +0530

    Add loader and error messages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 // MUI components
+import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
@@ -15,18 +17,27 @@ const emptyForm: Notification = {
 };
 
 function App() {
+  const [isSomeFieldsEmpty, setSomeFieldsEmpty] = useState<boolean>(false);
   const [form, setForm] = useState<Notification>({ ...emptyForm });
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [isError, setError] = useState<boolean>(false);
 
   const handleChange = (e: any) =>
     setForm((prev) => ({ ...prev, [e.target.id]: e.target.value }));
 
   const handleSubmit = async () => {
+    if (!form.body || !form.title) return setSomeFieldsEmpty(true);
+    setSomeFieldsEmpty(false);
+    setLoading(true);
+    setError(false);
     try {
       await axios.post('http://localhost:8080/broadcast', form);
       setForm({ ...emptyForm });
     } catch (error) {
       console.log(error);
+      setError(true);
     }
+    setLoading(false);
   };
 
   return (
@@ -43,6 +54,11 @@ function App() {
         <Typography mb={3}>
           Broadcast Notifications to connected users
         </Typography>
+        {isError && (
+          <Alert severity='error' variant='filled' sx={{ my: 2 }}>
+            Unable to broadcast the notification
+          </Alert>
+        )}
         <TextField
           id='title'
           value={form.title}
@@ -51,6 +67,7 @@ function App() {
           fullWidth
           margin='dense'
           size='small'
+          error={isSomeFieldsEmpty && !form.title}
         />
         <TextField
           id='body'
@@ -60,14 +77,28 @@ function App() {
           fullWidth
           margin='dense'
           size='small'
+          error={isSomeFieldsEmpty && !form.body}
         />
+        {isSomeFieldsEmpty && (
+          <Typography color='error' variant='body2' my={1}>
+            *Please fill in all the fields
+          </Typography>
+        )}
         <Button
+          disabled={isLoading}
           variant='contained'
           fullWidth
           sx={{ mt: 2 }}
           onClick={handleSubmit}
         >
-          Broadcast
+          {isLoading ? (
+            <>
+              <CircularProgress size={14} sx={{ mr: 1 }} />
+              Broadcasting...
+            </>
+          ) : (
+            'Broadcast'
+          )}
         </Button>
       </Container>
     </Box>
